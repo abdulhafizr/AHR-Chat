27c3ee5e765dd7cd68ddd852f10fcdd8b06c4c80
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-24 08:58:38 +0700

    Update firebase config

--- src/config/firebaseConfig/index.js
+++ src/config/firebaseConfig/index.js
@@ -4,13 +4,13 @@ import firebase from 'firebase/app';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDt-yJoCTNrM11W9NsYnoTYqsn3dfr1Bac",
-    authDomain: "ahr-chat-de2b8.firebaseapp.com",
-    projectId: "ahr-chat-de2b8",
-    storageBucket: "ahr-chat-de2b8.appspot.com",
-    messagingSenderId: "179796544020",
-    appId: "1:179796544020:web:bdf68dcc3c790a4a8bf501",
-    measurementId: "G-ENT3R4TVVN"
+apiKey: "AIzaSyDt-yJoCTNrM11W9NsYnoTYqsn3dfr1Bac",
+authDomain: "ahr-chat-de2b8.firebaseapp.com",
+projectId: "ahr-chat-de2b8",
+storageBucket: "ahr-chat-de2b8.appspot.com",
+messagingSenderId: "179796544020",
+appId: "1:179796544020:web:cf60ad792b21535d8bf501",
+measurementId: "G-PBGJP95S24"
 };
 
 firebase.initializeApp(firebaseConfig);
