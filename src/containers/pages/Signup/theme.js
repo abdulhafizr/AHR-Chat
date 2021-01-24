53de31409ffc92fea7018c437f469a24fc16c79f
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-01-24 15:51:23 +0700

    Finish make Signup Page

---
+++ src/containers/pages/Signup/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+    global: {
+        colors: {
+        // brand: '#051923'
+        },
+        font: {
+            family: 'Roboto',
+            size: '18px',
+            height: '20px',
+        },
+    },
+    icon: {
+        size: {
+            small: '18px',
+            sMedidum: '25px'
+        }
+    }
+}
+
+export default theme;
